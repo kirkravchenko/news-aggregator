d2f7a84eed404c5a5c5aa753ce207fe5076c7c19
silenchio <kyrylkravchenko1994@gmail.com>
2024-03-27 14:57:51 +0100

    added exception handling

--- bot.py
+++ bot.py
@@ -20,8 +20,21 @@ commands = [
     types.BotCommand(command=properties.URLS.economist_us.name, description=properties.Descriptions.economist_us.value),
     types.BotCommand(command=properties.URLS.economist_eu.name, description=properties.Descriptions.economist_eu.value),
     types.BotCommand(command=properties.URLS.economist_uk.name, description=properties.Descriptions.economist_uk.value),
+    types.BotCommand(command=properties.URLS.bloomberg_politics.name,description=properties.Descriptions.bloomberg_politics.value),
     types.BotCommand(command=properties.URLS.nyt_tech.name, description=properties.Descriptions.nyt_tech.value),
-    types.BotCommand(command=properties.URLS.bloomberg_politics.name, description=properties.Descriptions.bloomberg_politics.value)
+    types.BotCommand(command=properties.URLS.nyt_world.name, description=properties.Descriptions.nyt_world.value),
+    types.BotCommand(command=properties.URLS.nyt_africa.name, description=properties.Descriptions.nyt_africa.value),
+    types.BotCommand(command=properties.URLS.nyt_americas.name, description=properties.Descriptions.nyt_americas.value),
+    types.BotCommand(command=properties.URLS.nyt_asia_pacific.name, description=properties.Descriptions.nyt_asia_pacific.value),
+    types.BotCommand(command=properties.URLS.nyt_europe.name, description=properties.Descriptions.nyt_europe.value),
+    types.BotCommand(command=properties.URLS.nyt_mid_east.name, description=properties.Descriptions.nyt_mid_east.value),
+    types.BotCommand(command=properties.URLS.nyt_us_politics.name, description=properties.Descriptions.nyt_us_politics.value),
+    types.BotCommand(command=properties.URLS.nyt_personal_tech.name, description=properties.Descriptions.nyt_personal_tech.value),
+    types.BotCommand(command=properties.URLS.nyt_economy.name, description=properties.Descriptions.nyt_economy.value),
+    types.BotCommand(command=properties.URLS.nyt_your_money.name, description=properties.Descriptions.nyt_your_money.value),
+    types.BotCommand(command=properties.URLS.nyt_small_business.name, description=properties.Descriptions.nyt_small_business.value),
+    types.BotCommand(command=properties.URLS.nyt_science.name, description=properties.Descriptions.nyt_science.value),
+    types.BotCommand(command=properties.URLS.nyt_movies.name, description=properties.Descriptions.nyt_movies.value)
 ]
 bot.set_my_commands(commands)
 
@@ -35,13 +48,13 @@ def command_help(message):
         message.chat.id, types.MenuButtonCommands('commands')
     )
     if message.text[1:] == "remove":
-        print(message_ids)
         bot.delete_messages(message.chat.id, message_ids)
         message_ids.clear()
     elif message.text[1:] != "start":
         url = properties.URLS[message.text[1:]].value
         post = poster.form_post(url)
         message_ids.extend(poster.post_it(bot, post, message))
+
 
 print("bot is running")
 bot.infinity_polling()
